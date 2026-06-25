53e3abcad59043f0797442f0c0d9918154a343f6
Aleesha Afzal <aleeshaafzal112@gmail.com>
2026-06-25 15:00:35 +0500

    Initial backend commit

---
+++ models/Product.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: String,
+  image: String
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
---
+++ server.js
@@ -0,0 +1,49 @@
+const dns = require('dns')
+dns.setServers(['8.8.8.8', '8.8.4.4'])
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const Product = require('./models/Product')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected!'))
+  .catch((err) => console.log('MongoDB connection error:', err))
+
+app.get('/', (req, res) => {
+  res.send('Server is running!')
+})
+
+app.get('/api/products', async (req, res) => {
+  const products = await Product.find()
+  res.json(products)
+})
+
+app.post('/api/products', async (req, res) => {
+  const newProduct = new Product({
+    name: req.body.name,
+    price: req.body.price
+  })
+  await newProduct.save()
+  res.json(newProduct)
+})
+app.delete('/api/products/:id', async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id)
+  res.json({ message: 'Product deleted' })
+})
+app.put('/api/products/:id', async (req, res) => {
+  const updatedProduct = await Product.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name, price: req.body.price, image: req.body.image },
+    { new: true }
+  )
+  res.json(updatedProduct)
+})
+app.listen(5000, () => {
+  console.log('Server running on http://localhost:5000')
+})
